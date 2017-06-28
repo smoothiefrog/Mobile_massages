5f5e004c74c894e5f1aaa5e7b6bcd9f17dae2d0b
Roy33 <1270679840@qq.com>
2017-06-28 22:30:22 +0800

    2,4-1,4-2,success界面

---
+++ js/booking_page_2.js
@@ -0,0 +1,76 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2017-06-23 19:17:22
+ * @version $Id$
+ */
+$(document).ready(function(){
+	 $(".head-background").hide();
+ 	$(".times-circle-o").hide();
+ 	var flag=0;
+ 	$(".navbar-toggle").click(function() {
+ 		$(".navbar-toggle").css("background-color","#fff");
+
+ 		if(flag==0) {
+ 			$(".navbar").css("width","75%");
+ 			$(".navbar").css("margin-left","25%");
+
+ 			$(".head-title").css("margin-left","-15%");
+ 			$(".head-title-left").before("<h6>MENU</h6>");
+ 			$(".head-background").show();
+ 			flag=1;
+ 		}
+ 		else if(flag==1) {
+ 			flag=0;
+ 			$(".navbar").css("width","100%");
+ 			$(".navbar").css("margin-left","0%");
+ 			$(".navbar").css("height","0%");
+ 			$(".head-title").css({"margin-left":"10%"});
+ 			$("h6").remove();
+ 			$(".head-background").hide();
+ 		}
+ 	});
+	$(".therapist_img").click(function(){
+		  $(".therapist_img").css({"border":"none","margin":"0"});
+          $(this).css({"border":"7px solid #64d2a3","margin":"0 -7px 0 -7px"});
+        });
+// 	for(i=1;i<=8;i++){
+//   $(".therapist_"+i).click(function(){
+//     $(".therapist_intro").hide();
+//     $(".therapist_intro_"+i).show();
+//   });
+// }
+	$(".therapist_1").click(function(){
+		  $(".therapist_intro").hide();
+          $(".therapist_intro_1").show();
+        });
+	$(".therapist_2").click(function(){
+		  $(".therapist_intro").hide();
+          $(".therapist_intro_2").show();
+        });
+	$(".therapist_3").click(function(){
+		  $(".therapist_intro").hide();
+          $(".therapist_intro_3").show();
+        });
+	$(".therapist_4").click(function(){
+		  $(".therapist_intro").hide();
+          $(".therapist_intro_4").show();
+        });
+	$(".therapist_5").click(function(){
+		  $(".therapist_intro").hide();
+          $(".therapist_intro_5").show();
+        });
+	$(".therapist_6").click(function(){
+		  $(".therapist_intro").hide();
+          $(".therapist_intro_6").show();
+        });
+	$(".therapist_7").click(function(){
+		  $(".therapist_intro").hide();
+          $(".therapist_intro_7").show();
+        });
+	$(".therapist_8").click(function(){
+		  $(".therapist_intro").hide();
+          $(".therapist_intro_8").show();
+        });
+	
+});
