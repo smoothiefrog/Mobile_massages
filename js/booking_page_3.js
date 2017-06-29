2b2de09ff9e689509d1e9cdd3f349b9277983271
xsthhh <644596096@qq.com>
2017-06-29 20:23:40 +0800

    page1+3 over

--- js/booking_page_3.js
+++ js/booking_page_3.js
@@ -6,5 +6,38 @@ function init() {
 	$("#old_customer").click(function(){
 		window.location.href="booking_page_3-2.html";
 	})
+    //
+    $(".head-background").hide();
+ 	$(".times-circle-o").hide();
+ 	var flag=0;
+  $(".nav > li > a").click(function(){ 
+    for(i=0;i<8;i++) {
+      $(".nav > li > a").eq(i).css("background-color","white");
+    }
+    $(this).css("background-color","rgba(255,255,255,0.8)");
+  });
 
+  $(".navbar-toggle").click(function() {
+   $(".navbar-toggle").css("background-color","#fff");
+   if(flag==0) {
+      //$('.collapse').collapse();
+      //$(".collapse").collapse("troggle");
+      $(".navbar").css("width","75%");
+      $(".navbar").css("margin-left","25%");
+
+      $(".head-title").css("margin-left","-15%");
+      $(".head-title-left").before("<h6>MENU</h6>");
+      $(".head-background").show();
+      flag=1;
+    }
+    else if(flag==1) {
+      flag=0;
+      $(".navbar").css("width","100%");
+      $(".navbar").css("margin-left","0%");
+      $(".navbar").css("height","0%");
+      $(".head-title").css({"margin-left":"10%"});
+      $("h6").remove();
+      $(".head-background").hide();
+    }
+  });
 }
