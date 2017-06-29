82b572151106f35ae5add066809760414e9d1738
ZHEJIJI <459387043@qq.com>
2017-06-29 10:49:01 +0800

    js

--- js/home.js
+++ js/home.js
@@ -8,68 +8,81 @@
  $(init)
  function init() {
  	$(".head-background").hide();
+
  	$(".times-circle-o").hide();
  	var flag=0;
- 	$(".navbar-toggle").click(function() {
- 		$(".navbar-toggle").css("background-color","#fff");
+  $(".nav > li > a").click(function(){ 
+    for(i=0;i<8;i++) {
+      $(".nav > li > a").eq(i).css("background-color","white");
+    }
+    $(this).css("background-color","rgba(255,255,255,0.8)");
+  });
 
- 		if(flag==0) {
- 			$(".navbar").css("width","75%");
- 			$(".navbar").css("margin-left","25%");
+  $(".navbar-toggle").click(function() {
+   $(".navbar-toggle").css("background-color","#fff");
+   if(flag==0) {
+      //$('.collapse').collapse();
+      //$(".collapse").collapse("troggle");
+      $(".navbar").css("width","75%");
+      $(".navbar").css("margin-left","25%");
 
- 			$(".head-title").css("margin-left","-15%");
- 			$(".head-title-left").before("<h6>MENU</h6>");
- 			$(".head-background").show();
- 			flag=1;
- 		}
- 		else if(flag==1) {
- 			flag=0;
- 			$(".navbar").css("width","100%");
- 			$(".navbar").css("margin-left","0%");
- 			$(".navbar").css("height","0%");
- 			$(".head-title").css({"margin-left":"10%"});
- 			$("h6").remove();
- 			$(".head-background").hide();
- 		}
- 	});
- 	$(".nav-home").click(function() {  
-      $("html,body").animate({  
-        scrollTop: $(".home-top").offset().top 
-      }, 200); 
-    });
-    $("html,body").animate({
-    		scrollTop:$("#home-booking").offset().top
-    	},200);
-    $(".nav-therapist").click(function (){  
-      $("html,body").animate({  
-        scrollTop: $(".home-therapist").offset().top 
-      }, 200); 
-    }); 
-    $(".nav-styles").click(function (){  
-      $("html,body").animate({  
-        scrollTop: $(".home-type").offset().top 
-      }, 200); 
-    }); 
-    $(".nav-pricing").click(function (){  
-      $("html,body").animate({  
-        scrollTop: $(".home-pricing").offset().top 
-      }, 200); 
-    }); 
-    $(".nav-faq").click(function (){  
-      $("html,body").animate({  
-        scrollTop: $(".home-faq").offset().top 
-      }, 200); 
-    }); 
-    $(".nav-contact").click(function (){  
-      $("html,body").animate({  
-        scrollTop: $(".home-contact").offset().top 
-      }, 200); 
-    }); 
+      $(".head-title").css("margin-left","-15%");
+      $(".head-title-left").before("<h6>MENU</h6>");
+      $(".head-background").show();
+      flag=1;
+    }
+    else if(flag==1) {
+      flag=0;
+      $(".navbar").css("width","100%");
+      $(".navbar").css("margin-left","0%");
+      $(".navbar").css("height","0%");
+      $(".head-title").css({"margin-left":"10%"});
+      $("h6").remove();
+      $(".head-background").hide();
+    }
+  });
 
-    
-   
- 	$("select").click(function() {
- 		$("#_input").val($("#select option:selected").text());
- 	});
- 	
- }
+
+  $(".nav-home-home").click(function() {  
+    $("html,body").animate({  
+      scrollTop: $(".home-top").offset().top 
+    }, 200); 
+  });
+  $(".nav-booking").click(function() {  
+    $("html,body").animate({  
+      scrollTop: $(".home-booking").offset().top 
+    }, 200); 
+  });
+  $(".nav-therapist").click(function (){  
+    $("html,body").animate({  
+      scrollTop: $(".home-therapist").offset().top 
+    }, 200); 
+  }); 
+  $(".nav-styles").click(function (){  
+    $("html,body").animate({  
+      scrollTop: $(".home-type").offset().top 
+    }, 200); 
+  }); 
+  $(".nav-pricing").click(function (){  
+    $("html,body").animate({  
+      scrollTop: $(".home-pricing").offset().top 
+    }, 200); 
+  }); 
+  $(".nav-faq").click(function (){  
+    $("html,body").animate({  
+      scrollTop: $(".home-faq").offset().top 
+    }, 200); 
+  }); 
+  $(".nav-contact").click(function (){  
+    $("html,body").animate({  
+      scrollTop: $(".home-contact").offset().top 
+    }, 200); 
+  }); 
+
+
+  
+  $("select").click(function() {
+   $("#_input").val($("#select option:selected").text());
+ });
+  
+}
