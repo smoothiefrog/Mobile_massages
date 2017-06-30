1e7d27080d3628be95c63850e1f154072e996ec3
Roy33 <1270679840@qq.com>
2017-06-30 09:54:52 +0800

    -a

--- js/booking_page_2.js
+++ js/booking_page_2.js
@@ -5,31 +5,66 @@
  * @version $Id$
  */
 $(document).ready(function(){
-	 $(".head-background").hide();
+	 // $(".head-background").hide();
+ 	// $(".times-circle-o").hide();
+ 	// var flag=0;
+ 	// $(".navbar-toggle").click(function() {
+ 	// 	$(".navbar-toggle").css("background-color","#fff");
+
+ 	// 	if(flag==0) {
+ 	// 		$(".navbar").css("width","75%");
+ 	// 		$(".navbar").css("margin-left","25%");
+
+ 	// 		$(".head-title").css("margin-left","-15%");
+ 	// 		$(".head-title-left").before("<h6>MENU</h6>");
+ 	// 		$(".head-background").show();
+ 	// 		flag=1;
+ 	// 	}
+ 	// 	else if(flag==1) {
+ 	// 		flag=0;
+ 	// 		$(".navbar").css("width","100%");
+ 	// 		$(".navbar").css("margin-left","0%");
+ 	// 		$(".navbar").css("height","0%");
+ 	// 		$(".head-title").css({"margin-left":"10%"});
+ 	// 		$("h6").remove();
+ 	// 		$(".head-background").hide();
+ 	// 	}
+ 	// });
+ 	$(".head-background").hide();
+
  	$(".times-circle-o").hide();
  	var flag=0;
- 	$(".navbar-toggle").click(function() {
- 		$(".navbar-toggle").css("background-color","#fff");
+  $(".nav > li > a").click(function(){ 
+    for(i=0;i<8;i++) {
+      $(".nav > li > a").eq(i).css("background-color","white");
+    }
+    $(this).css("background-color","rgba(255,255,255,0.8)");
+  });
 
- 		if(flag==0) {
- 			$(".navbar").css("width","75%");
- 			$(".navbar").css("margin-left","25%");
+  $(".navbar-toggle").click(function() {
+   $(".navbar-toggle").css("background-color","#fff");
+   if(flag==0) {
+      //$('.collapse').collapse();
+      //$(".collapse").collapse("troggle");
+      $(".navbar").css("width","75%");
+      $(".navbar").css("margin-left","25%");
 
- 			$(".head-title").css("margin-left","-15%");
- 			$(".head-title-left").before("<h6>MENU</h6>");
- 			$(".head-background").show();
- 			flag=1;
- 		}
- 		else if(flag==1) {
- 			flag=0;
- 			$(".navbar").css("width","100%");
- 			$(".navbar").css("margin-left","0%");
- 			$(".navbar").css("height","0%");
- 			$(".head-title").css({"margin-left":"10%"});
- 			$("h6").remove();
- 			$(".head-background").hide();
- 		}
- 	});
+      $(".head-title").css("margin-left","-15%");
+      $(".head-title-left").before("<h6>MENU</h6>");
+      $(".head-background").show();
+      flag=1;
+    }
+    else if(flag==1) {
+      flag=0;
+      $(".navbar").css("width","100%");
+      $(".navbar").css("margin-left","0%");
+      $(".navbar").css("height","0%");
+      $(".head-title").css({"margin-left":"10%"});
+      $("h6").remove();
+      $(".head-background").hide();
+    }
+  });
+
 	$(".therapist_img").click(function(){
 		  $(".therapist_img").css({"border":"none","margin":"0"});
           $(this).css({"border":"7px solid #64d2a3","margin":"0 -7px 0 -7px"});
